2c407818c25c782b607d5b8ef8753ad9958db6e8
Taylor Lacerte <taylorlacerte@Taylors-MacBook-Pro.local>
2019-08-16 10:03:50 -0500

    flight arrival time renders on details page

--- controllers/details.js
+++ controllers/details.js
@@ -1,23 +1,14 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-    create,
-    addArrivalInfo
+    create
 };
 
 function create(req, res){
     Flight.findById(req.params.id, function(err, flight){
-        flight.details.push(req.body);
+        flight.destinations.push(req.body);
         flight.save(function(err){
             res.redirect(`/flights/${flight._id}`);
         });
-    });
-}
-
-function addArrivalInfo(req, res){
-    var arrivalInfo = new Flight(req.body);
-    arrivalInfo.save(function(err){
-        if (err) return res.render(`/flights/${flight._id}`);
-        res.redirect(`/flights/${flight._id}`);    
     });
 }
